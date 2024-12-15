dc9543d722d0e8da32d5297d83cf46b3290ed29b
Bibek Regmi <vivekregmi83@gmail.com>
2024-12-15 13:33:07 +0545

    pointers in go

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-func changeNum(num int) {
-	num = 5
+// by value
+// func changeNum(num int) {
+// 	num = 5
+// 	fmt.Println("in change num", num)
+// }
+
+// by reference
+func changeNum(num *int) {
+	*num = 5
 	fmt.Println("in change num", num)
 }
 
 func main() {
 	num := 1
 	fmt.Println("Memory ", &num)
-	changeNum(num)
+	changeNum(&num)
 	fmt.Println("After change num in main", num)
 }
